a37c657d7b038b03f6c3991ec287419c45cf4c91
Gonçalo Pataca <gonzalo.nascimento.pataca@gmail.com>
2025-04-18 15:49:42 +0100

    Start with fresh custom eslint flat config (#46)

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename="/memory-card-game">
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+const rootElement = document.querySelector("#root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter basename="/memory-card-game">
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+} else {
+  console.error("Root element not found");
+}
